1359b61a2f1a2c5abb4156169fb3eb5eddcaf706
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2023-03-03 12:00:33 -0300

    Código da questão 12

--- ListaResolvida/q_12/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_12/src/br/edu/principal/Principal.java
@@ -5,13 +5,13 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite um número: ");
+		System.out.println("Digite um nï¿½mero: ");
 		int num1 = sc.nextInt();
-		System.out.println("Digite outro número: ");
+		System.out.println("Digite outro nï¿½mero: ");
 		int num2 = sc.nextInt();
 		double r1 = Math.pow(num1, num2);
 		double r2 = Math.pow(num2, num1);
-		System.out.println("O primeiro número elevado ao segundo é igual a: "+r1+"\nE o segundo número elevado ao primeiro é igual a: "+r2);
+		System.out.println("O primeiro nÃºmero elevado ao segundo Ã© igual a: "+r1+"\nE o segundo nÃºmero elevado ao primeiro Ã© igual a: "+r2);
 
 	}
 
