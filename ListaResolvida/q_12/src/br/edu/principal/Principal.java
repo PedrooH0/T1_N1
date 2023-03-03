15b48cd6ca78fd26973679eb25ae4fc2e083b803
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2023-03-03 12:03:33 -0300

    Código q12

--- ListaResolvida/q_12/src/br/edu/principal/Principal.java
+++ ListaResolvida/q_12/src/br/edu/principal/Principal.java
@@ -5,9 +5,9 @@ public class Principal {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Digite um n�mero: ");
+		System.out.println("Digite um número: ");
 		int num1 = sc.nextInt();
-		System.out.println("Digite outro n�mero: ");
+		System.out.println("Digite outro número: ");
 		int num2 = sc.nextInt();
 		double r1 = Math.pow(num1, num2);
 		double r2 = Math.pow(num2, num1);
