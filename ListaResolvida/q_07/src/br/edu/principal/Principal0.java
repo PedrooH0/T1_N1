613c1a3405153222cdb44d82e6add41bc1d30118
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2023-03-05 11:22:58 -0300

    alteração da classe

--- ListaResolvida/q_07/src/br/edu/principal/Principal0.java
+++ ListaResolvida/q_07/src/br/edu/principal/Principal0.java
@@ -1,6 +1,6 @@
 package br.edu.principal;
 import java.util.Scanner;
-public class Principal {
+public class Principal0 {
 
 	public static void main(String[] args) {
 		
--- ListaResolvida/q_09/src/br/edu/principal/Principal1.java
+++ ListaResolvida/q_09/src/br/edu/principal/Principal1.java
@@ -7,9 +7,9 @@ public class Principal1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Calculadora da área de um triângulo (em metros).");
-		System.out.println("Digite o valor da altura do triângulo");
+		System.out.println("Digite o valor da altura do triângulo: ");
 		float alt = sc.nextFloat();
-		System.out.println("Digite o valor da base do triângulo");
+		System.out.println("Digite o valor da base do triângulo: ");
 		float base = sc.nextFloat();
 		float area = (base + alt)/2;
 		System.out.println("O valor da área desse triângulo é de "+area+" metros quadrados.");
