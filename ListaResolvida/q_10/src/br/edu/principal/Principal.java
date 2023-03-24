dde34d4f3f7e7123f943eabe105849e1f82cb3d7
Pedro Henrique <124939591+PedrooH0@users.noreply.github.com>
2023-03-24 14:27:38 -0300

    Questões Resolvidas

---
+++ ListaResolvida/q_10/src/br/edu/principal/Principal.java
@@ -0,0 +1,19 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		double area;
+		double raio;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Calculo da área de um círculo (em metros).");
+		System.out.println("Digite o valor do raio desse círculo: ");
+		raio = sc.nextDouble();
+		area = 3.1415 * Math.pow(raio, 2);
+		System.out.println("A área desse círculo será de "+area+" metros quadrados.");
+
+	}
+
+}
